827f119808301c8a588cbcdfadb12ffd3362a340
dishikatalreja <dishikatalreja11@gmail.com>
2023-08-16 02:09:40 +0530

    code with error

---
+++ android/app/src/main/kotlin/com/example/climatrial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.climatrial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/weather.dart
@@ -0,0 +1,50 @@
+import 'package:climatrial/services/location.dart';
+import 'package:climatrial/services/networking.dart';
+import 'package:flutter/material.dart';
+
+const apiKey = 'f54882d423a394dba2e8075a71d3c408';
+
+class WeatherModel {
+
+  dynamic getCurrentLocation() async {
+
+    Location location = Location();
+    await location.getCurrentLocation();
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+    dynamic weatherData = await networkHelper.getData();
+    debugPrint("weatherData: $weatherData");
+    return weatherData;
+  }
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
